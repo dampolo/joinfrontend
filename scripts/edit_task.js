575ae05762f2149964ba8f028da4aa39b40bd411
Damian Poloczek <damianpoloczek@gmail.com>
2025-05-17 19:47:54 +0200

    dataset with comment

--- scripts/edit_task.js
+++ scripts/edit_task.js
@@ -282,7 +282,7 @@ function editTaskAssignedTo() {
 
   for (let i = 0; i < checkBoxes.length; i++) {    
     if (checkBoxes[i].checked) {
-      const contact = checkBoxes[i].value;      
+      const contact = checkBoxes[i].value;
       const id = checkBoxes[i].dataset.userId;      //dataset Because we need other name for ID to work with checkbox
 
       newTask.assigned_to.push({
